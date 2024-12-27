889103310eecd41d2524499fafb8fe4a4c1bc09e
Kevin Mateo Simbaña Merchán <mateomerchan1@outlook.com>
2024-12-27 20:25:42 +0100

    Diseño Final del login y registrar

--- logear/src/app/app.routes.ts
+++ logear/src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { RegisterComponent } from './register/register.component';
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RouterModule } from '@angular/router';
+import { CarruselComponent } from './carrusel/carrusel.component';
 const routeConfig: Routes = [
   { path: "", redirectTo:"/login", pathMatch: "full" },
   { path: "login", component: LoginComponent, pathMatch: "full" },
   { path: "register", component: RegisterComponent, pathMatch: "full" },
   {path: "home", component: HomeComponent, pathMatch: "full"},
+  {path: "carrusel", component: CarruselComponent, pathMatch: "full"},
 ];
 export default routeConfig;
